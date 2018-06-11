39135d8459823f11670eb6554de9f677fa33d78f
caohui <772071248@qq.com>
2018-06-12 01:06:12 +0800

    init project

--- www/index.php
+++ www/index.php
@@ -1,4 +1,18 @@
 <?php 
 
-phpinfo();
-?>
+// phpinfo();
+
+// $redis = new Redis();
+
+// $redis->connect('192.168.1.104', '6379');
+
+// $redis->auth('123456');
+
+// $redis->set('a', 1);
+
+// echo $redis->get('a');
+
+$conn = new mysqli('192.168.1.104', 'root', '123456', 'mysql');
+
+var_dump($conn);
+
